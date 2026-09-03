3a978d606482f550585a1d326a01f05de9461727
theassetportal <117767574+theassetportal@users.noreply.github.com>
2026-09-03 13:55:19 -0600

    ejs

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+ 
 // Define the application environment
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+ 
 const app = express();
-
-/**
-  * Configure Express middleware
-  */
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-/**
-  * Routes
-  */
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+ 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+ 
+app.get("/", async (req, res) => {
+	const title = "Home";
+	res.render("home", { title });
 });
-
-app.get('/organizations', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+ 
+app.get("/organizations", async (req, res) => {
+	const title = "Our partner organizations";
+	res.render("organizations", { title });
 });
-
-app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+ 
+app.get("/categories", async (req, res) => {
+	const title = "Service Categories";
+	res.render("categories", { title });
 });
-
+ 
+app.get("/projects", async (req, res) => {
+	const title = "Service Projects";
+	res.render("projects", { title });
+});
+ 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+	console.log(`Server is running at http://127.0.0.1:${PORT}`);
+	console.log(`Environment: ${NODE_ENV}`);
 });
