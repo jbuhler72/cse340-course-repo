0ede4ac1a1666edbe1fcffe1e0264d91fbecad31
theassetportal <117767574+theassetportal@users.noreply.github.com>
2026-09-09 20:24:27 -0600

    database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
  
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
@@ -22,9 +23,11 @@ app.get("/", async (req, res) => {
 	res.render("home", { title });
 });
  
-app.get("/organizations", async (req, res) => {
-	const title = "Our partner organizations";
-	res.render("organizations", { title });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
 });
  
 app.get("/categories", async (req, res) => {
