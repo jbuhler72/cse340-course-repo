330cc5ba528988ebab6f8ee26ec87129000ead37
theassetportal <117767574+theassetportal@users.noreply.github.com>
2026-09-10 16:01:26 -0600

    categories

---
+++ src/models/categories.js
@@ -0,0 +1,17 @@
+import db from './db.js'
+
+const getAllCategories = async() => {
+    const query = `
+     SELECT category_id, name,description
+        FROM category
+        ORDER BY category_id;
+
+        
+    `;
+
+    const result = await db.query(query);
+
+    return result.rows;
+}
+
+export {getAllCategories}
